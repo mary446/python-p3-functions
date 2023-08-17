b3fc1e6a347b4440b65019ca7e59aca7e61e6509
mary446 <135317707+mary446@users.noreply.github.com>
2023-08-17 11:39:18 +0300

    python functions passed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
+
     pass
+    
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Mary")
+pass
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+pass
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+add (2, 2)
+pass
 
 def halve(number):
-    pass
+    return number/2
+halve(8)
+pass
